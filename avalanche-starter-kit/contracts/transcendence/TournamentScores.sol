8050c64930ac1ebba1a0c3149602c1c8ce3b07b2
n1smm <tjaz.juvan@gmail.com>
2025-08-26 18:44:01 +0200

    contracts not tested; almost finished
    need to implement some security isues regarding uploading data to chain
    (so that server needs too give permission to add the thing to chain)

--- avalanche-starter-kit/contracts/transcendence/TournamentScores.sol
+++ avalanche-starter-kit/contracts/transcendence/TournamentScores.sol
@@ -2,10 +2,11 @@
 
 pragma solidity 0.8.25;
 
-
+	//get all the tournaments player was in //TODO
 contract TournamentScores {
 
 	//events
+	event tournamentDataSaved(string msg, uint256 tournament_id);
 
 	//errors
 	error numberToHigh(string msg, uint256 NofPlayers);
@@ -14,18 +15,17 @@ contract TournamentScores {
 	//structs
 	struct tournament {
 		uint256[8]	user_ids;
-		string[8]	usernames;
 		address[8]	user_wallets;
 		uint256[8] 	user_scores;
-		uint256		winner_id;
-		string		winner_name;
-		uint256		match_id;
+		uint256[8]	winner_ids;
+		string		winner_names;
 		uint256		tournament_id;
 	}
 
 
 	//vars
-	tournament[]			public	tournament_registry;
+	// tournament[]			public	tournament_registry;
+	mapping(uint256 => tournament) public tournament_registry;
 	uint256					private _current_tournament; //when a turnament is played it will have this index	 
 
 	//constructor
@@ -36,43 +36,34 @@ contract TournamentScores {
 	function saveTournamentData(
 		uint256				_NofPlayers,	
 		uint256[8]	memory	_user_ids,
-		string[8]	memory	_usernames,
 		uint256[8]	memory	_user_scores,
-		address[8]	memory	_user_wallets,
-		uint256				_winner_id,
-		string		memory	_winner_name,
-		uint256				_match_id,
+		uint256[8]	memory	_winner_ids,
+		string		memory	_winner_names,
 		uint256				_tournament_id
 	)	public {
 
 		if (_NofPlayers > 8)
 			revert numberToHigh("Too many players", _NofPlayers);
 
-		tournament_registry.push();
-		tournament storage t = tournament_registry[_current_tournament];
+		tournament storage t = tournament_registry[_tournament_id];
 		
 		for (uint256 i = 0; i < _NofPlayers; i++) {
 			t.user_ids[i] = _user_ids[i];
-			t.usernames[i] = _usernames[i];
 			t.user_scores[i] = _user_scores[i];
-			t.user_wallets[i] = _user_wallets[i];
 		}
-		t.winner_id = _winner_id;
-		t.winner_name = _winner_name;
-		t.match_id = _match_id;
+		t.winner_ids = _winner_ids;
+		t.winner_names = _winner_names;
 		t.tournament_id = _tournament_id;
 		_current_tournament++;
+		emit tournamentDataSaved("all neccesarry data for a tournament has been saved. tournament id: ", _tournament_id);
 	}
 
 	//returns a tournament with specified idx as a tuple (can be parsed as JSON in frontend)
 	function getTournamentData(uint256 tournamentIndex) public view returns (
 		uint256[8] memory,
-		string[8] memory,
-		address[8] memory,
 		uint256[8] memory,
-		uint256,
+		uint256[8] memory,
 		string memory,
-		uint256,
 		uint256
 	) {
 
@@ -82,18 +73,14 @@ contract TournamentScores {
 		tournament memory t = tournament_registry[tournamentIndex];
 		return (
 			t.user_ids,
-			t.usernames,
-			t.user_wallets,
 			t.user_scores,
-			t.winner_id,
-			t.winner_name,
-			t.match_id,
+			t.winner_ids,
+			t.winner_names,
 			t.tournament_id
 		);
 	}
 
-
-	//get all the tournaments player was in
+	//get all the tournaments player was in //TODO
 
 
 }
